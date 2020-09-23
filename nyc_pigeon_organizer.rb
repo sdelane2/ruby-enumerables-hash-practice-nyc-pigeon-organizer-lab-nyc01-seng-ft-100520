5d5c3b2139f0b9d6b6df81860de4080002503683
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-23 06:48:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,9 @@ def nyc_pigeon_organizer(data)
       name_array.each do |name|
         if !final_array[name]
           final_array[name] = {}
-        binding.pry
+        end
+        if !final_array[inner_key]
+          final_array[inner_key]
       end
     end
   end
