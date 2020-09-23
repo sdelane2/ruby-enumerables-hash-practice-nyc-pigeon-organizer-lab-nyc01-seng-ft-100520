3081b63a452ce2f112b8b9149e79dad2063f5c72
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-23 06:40:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inner_key, name|
+      inner_key 
     binding.pry
     end
   end
